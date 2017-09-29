f7e3f0d626e7aeabf44a2ba2e58b46ed26deb7c3
Arnom Maciel <arnom.silva@sgr.com.br>
2017-09-29 10:40:59 -0300

    Initial charge

---
+++ index.android.js
@@ -0,0 +1,13 @@
+var React = require('react');
+var Text = require('react-native').Text;
+var AppRegistry = require('react-native').AppRegistry;
+
+const App = function(){
+    return (
+        <Text>Meu App</Text>
+    );
+};
+
+AppRegistry.registerComponent('app1',function(){ 
+    return App; 
+});
